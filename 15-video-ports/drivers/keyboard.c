a891ecc08e4ebb7047150255ccd3b9dafe5b7c8a
yupeng <>
2021-07-15 07:33:54 +0800

    print keypresses as ascii on screen

--- 15-video-ports/drivers/keyboard.c
+++ 15-video-ports/drivers/keyboard.c
@@ -3,10 +3,33 @@
 #include "ports.h"
 #include "vga.h"
 
+char sc_to_ascii[56] = {
+'\0',   /* key detection error or internal buffer overrun */
+'\0',  /* escape */
+/* 0x02 */
+'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+'\b',   /* 0x0f: backspace */
+'\t',   /* tab */
+/* 0x10 */
+'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+'\n',   /* 0x1c: enter */
+'\0',   /* left ctrl */
+/* 0x1e */
+'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+'\0',   /* 0x2a: left shift */
+'\\',   /* 0x2b: backslash */
+/* 0x2c */
+'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+'\0',   /* 0x36: right shift */
+};
+
+
 static
 void keyboard_irq_handler(registers_t regs) {
     uint8_t scan_code = port_byte_in(0x60);
-    vga_print_dec(scan_code);
+    char ascii_char = sc_to_ascii[scan_code];
+    char str[2] = {ascii_char, '\0'};
+    vga_print(str);
 }
 
 void keyboard_init(void) {
